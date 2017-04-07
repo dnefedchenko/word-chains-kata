d65f1e2f3994d7db495fcd8fd2b9e9492fb3d0e2
dnefedchenko <dnefedchenko@gmail.com>
2017-04-07 12:00:52 +0300

    Mavenize java project

---
+++ src/main/java/com/freeman/Test.java
@@ -0,0 +1,9 @@
+package com.freeman;
+/**
+ * Created by freeman on 07.04.17.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hi, there!");
+    }
+}
